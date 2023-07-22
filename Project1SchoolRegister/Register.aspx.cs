87dc172cb2ec117280c30b9422a59e797c6a3cbb
ömer dikişli <omerdksli@gmail.com>
2023-07-22 04:24:25 +0300

    add database

---
+++ Project1SchoolRegister/Register.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project1SchoolRegister
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBLSTUDENTTableAdapter dt = new DataSet1TableAdapters.TBLSTUDENTTableAdapter();
+            dt.AddStudent(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text);
+
+        }
+    }
+}
